1db3ac4dbed3c35db4ad228510e9ab7dda22fc59
josie <jjanssen@mmm.edu>
2019-05-02 11:40:29 -0400

    main

---
+++ FinalGame/Assets/scenetransition.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class scenetransition : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0)) {
+            SceneManager.LoadScene(0); }
+    }
+
+   public void StartGame()
+    {
+        SceneManager.LoadScene(0);
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        SceneManager.LoadScene(0);
+    }
+}
